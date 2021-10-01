b218c8476d20e890db4995fced26ad9e3c4fe3f6
Douglas Byers <djbyers3438@gmail.com>
2021-10-01 09:25:47 -0500

    updated files

---
+++ .ipynb_checkpoints/WheelofFortune-checkpoint.py
@@ -0,0 +1,19 @@
+#Store the word in an answer variable in all upper case
+
+answer = "Nashville".upper
+
+#Store hint
+
+hint = "American Sports Cities"
+
+#Store guessed word as a mask of underscores
+
+guessed = list("_" * len(answer))
+
+#Create player list
+
+player_list = {
+    "player1": 0,
+    "player2": 0,
+    "player3": 0
+}
